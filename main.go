05474904f9fa5dbbd676f78cf3db0b2cba06662a
gilhyeon11 <rlagus315@gmail.com>
2026-03-22 23:47:46 +0900

    commit

--- main.go
+++ main.go
@@ -1,122 +1,67 @@
 package main
 
 import (
+	"image"
 	"image/color"
+	_ "image/png"
 	"log"
+	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/audio"
+	"github.com/hajimehoshi/ebiten/v2/audio/vorbis"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
+type GameState int
+
+const (
+	StateMenu GameState = iota
+	StatePlaying
+)
+
 var (
-	playerSprites [4]*ebiten.Image
-	bgImage       *ebiten.Image
+	playerSpriteBoy  *ebiten.Image
+	playerSpriteGirl *ebiten.Image
+	tilesetImage     *ebiten.Image
+	audioContext     *audio.Context
+	bgmPlayer        *audio.Player
+)
+
+const (
+	mapWidth  = 20
+	mapHeight = 15
+	tileSize  = 32
 )
 
 func init() {
-	// Initialize the 4 directional sprites
-	for i := 0; i < 4; i++ {
-		playerSprites[i] = createPlayerImage(i)
+	var err error
+	playerSpriteBoy, _, err = ebitenutil.NewImageFromFile("assets/graphics/characters/boy_run.png")
+	if err != nil {
+		log.Printf("Failed to load boy sprite: %v", err)
 	}
-	// Initialize the town background
-	bgImage = createTownBackground()
-}
 
-// createTownBackground procedurally generates a simple 2D town background
-func createTownBackground() *ebiten.Image {
-	img := ebiten.NewImage(320, 240)
+	playerSpriteGirl, _, err = ebitenutil.NewImageFromFile("assets/graphics/characters/girl_run.png")
+	if err != nil {
+		log.Printf("Failed to load girl sprite: %v", err)
+	}
 
-	// Grass (Medium Sea Green)
-	img.Fill(color.NRGBA{60, 179, 113, 255})
+	tilesetImage, _, err = ebitenutil.NewImageFromFile("assets/graphics/tilesets/Outside.png")
+	if err != nil {
+		log.Printf("Failed to load tileset: %v", err)
+	}
 
-	drawRect := func(x, y, w, h int, c color.Color) {
-		for i := 0; i < w; i++ {
-			for j := 0; j < h; j++ {
-				if x+i >= 0 && x+i < 320 && y+j >= 0 && y+j < 240 {
-					img.Set(x+i, y+j, c)
-				}
-			}
+	audioContext = audio.NewContext(44100)
+	f, err := os.Open("assets/audio/bgm/Title.ogg")
+	if err == nil {
+		s, err := vorbis.DecodeWithSampleRate(44100, f)
+		if err == nil {
+			loop := audio.NewInfiniteLoop(s, s.Length())
+			bgmPlayer, _ = audioContext.NewPlayer(loop)
+			bgmPlayer.Play()
 		}
 	}
-
-	// Dirt paths (Dark Goldenrod)
-	dirt := color.RGBA{184, 134, 11, 255}
-	drawRect(0, 100, 320, 40, dirt) // horizontal
-	drawRect(140, 0, 40, 240, dirt) // vertical
-
-	// Draw house helper
-	drawHouse := func(bx, by int) {
-		walls := color.RGBA{222, 184, 135, 255} // Burlywood
-		drawRect(bx, by, 60, 40, walls)
-		roof := color.RGBA{178, 34, 34, 255}    // FireBrick
-		drawRect(bx-4, by-10, 68, 20, roof)
-		door := color.RGBA{139, 69, 19, 255}    // SaddleBrown
-		drawRect(bx+20, by+20, 20, 20, door)
-		win := color.RGBA{135, 206, 235, 255}   // SkyBlue
-		drawRect(bx+8, by+10, 12, 12, win)
-		drawRect(bx+40, by+10, 12, 12, win)
-	}
-
-	// Draw 4 houses
-	drawHouse(40, 40)
-	drawHouse(220, 40)
-	drawHouse(40, 160)
-	drawHouse(220, 160)
-
-	return img
-}
-
-// createPlayerImage procedurally generates a simple 2D pixel art character.
-// dir: 0=down, 1=up, 2=left, 3=right
-func createPlayerImage(dir int) *ebiten.Image {
-	img := ebiten.NewImage(32, 32)
-
-	skin := color.RGBA{255, 204, 153, 255}
-	shirt := color.RGBA{0, 102, 204, 255}
-	pants := color.RGBA{40, 40, 40, 255}
-	hair := color.RGBA{100, 50, 0, 255}
-	eyeColor := color.RGBA{0, 0, 0, 255}
-
-	drawRect := func(x, y, w, h int, c color.Color) {
-		for i := 0; i < w; i++ {
-			for j := 0; j < h; j++ {
-				img.Set(x+i, y+j, c)
-			}
-		}
-	}
-
-	if dir == 1 { // Up (Back view)
-		drawRect(8, 4, 16, 16, hair) // Full hair head
-		drawRect(10, 20, 12, 8, shirt)
-		drawRect(11, 28, 4, 4, pants)
-		drawRect(17, 28, 4, 4, pants)
-		return img
-	}
-
-	// Head
-	drawRect(8, 4, 16, 16, skin)
-	// Hair
-	drawRect(8, 2, 16, 4, hair)
-	drawRect(7, 4, 2, 10, hair)
-	drawRect(23, 4, 2, 10, hair)
-
-	// Body
-	drawRect(10, 20, 12, 8, shirt)
-	// Legs
-	drawRect(11, 28, 4, 4, pants)
-	drawRect(17, 28, 4, 4, pants)
-
-	// Eyes
-	if dir == 0 { // Down
-		drawRect(11, 10, 2, 4, eyeColor)
-		drawRect(19, 10, 2, 4, eyeColor)
-	} else if dir == 2 { // Left
-		drawRect(9, 10, 2, 4, eyeColor)
-	} else if dir == 3 { // Right
-		drawRect(21, 10, 2, 4, eyeColor)
-	}
-
-	return img
 }
 
 type Rect struct {
@@ -124,21 +69,17 @@ type Rect struct {
 }
 
 var houses = []Rect{
-	{36, 30, 68, 50},  // House 1
-	{216, 30, 68, 50}, // House 2
-	{36, 150, 68, 50}, // House 3
-	{216, 150, 68, 50},// House 4
+	{36, 30, 68, 50},
+	{400, 30, 68, 50},
+	{36, 350, 68, 50},
+	{400, 350, 68, 50},
 }
 
 func isColliding(x, y float64) bool {
 	pw, ph := 32.0, 32.0
-
-	// Screen bounds
-	if x < 0 || x+pw > 320 || y < 0 || y+ph > 240 {
+	if x < 0 || x+pw > mapWidth*tileSize || y < 0 || y+ph > mapHeight*tileSize {
 		return true
 	}
-
-	// Houses
 	for _, h := range houses {
 		if x < h.X+h.W && x+pw > h.X && y < h.Y+h.H && y+ph > h.Y {
 			return true
@@ -148,53 +89,155 @@ func isColliding(x, y float64) bool {
 }
 
 type Game struct {
-	x, y float64
-	dir  int // 0: down, 1: up, 2: left, 3: right
+	state      GameState
+	isGirl     bool
+	x, y       float64
+	dir        int
+	step       int
+	tick       int
+	camX, camY float64
 }
 
 func (g *Game) Update() error {
-	// Toggle full screen
 	if inpututil.IsKeyJustPressed(ebiten.KeyF) {
 		ebiten.SetFullscreen(!ebiten.IsFullscreen())
 	}
 
+	if g.state == StateMenu {
+		if inpututil.IsKeyJustPressed(ebiten.KeyLeft) || inpututil.IsKeyJustPressed(ebiten.Key1) {
+			g.isGirl = false
+			g.state = StatePlaying
+		} else if inpututil.IsKeyJustPressed(ebiten.KeyRight) || inpututil.IsKeyJustPressed(ebiten.Key2) {
+			g.isGirl = true
+			g.state = StatePlaying
+		}
+		return nil
+	}
+
 	newX, newY := g.x, g.y
+	moved := false
 
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
 		newX -= 2
-		g.dir = 2
+		g.dir = 1
+		moved = true
 	} else if ebiten.IsKeyPressed(ebiten.KeyRight) {
 		newX += 2
-		g.dir = 3
+		g.dir = 2
+		moved = true
 	} else if ebiten.IsKeyPressed(ebiten.KeyUp) {
 		newY -= 2
-		g.dir = 1
+		g.dir = 3
+		moved = true
 	} else if ebiten.IsKeyPressed(ebiten.KeyDown) {
 		newY += 2
 		g.dir = 0
+		moved = true
 	}
 
-	// Try moving in X
+	if moved {
+		g.tick++
+		if g.tick > 8 {
+			g.step = (g.step + 1) % 4
+			g.tick = 0
+		}
+	} else {
+		g.step = 0
+	}
+
 	if newX != g.x && !isColliding(newX, g.y) {
 		g.x = newX
 	}
-	// Try moving in Y
 	if newY != g.y && !isColliding(g.x, newY) {
 		g.y = newY
+	}
+
+	g.camX = g.x - 160 + 16
+	g.camY = g.y - 120 + 24
+
+	if g.camX < 0 {
+		g.camX = 0
+	}
+	if g.camY < 0 {
+		g.camY = 0
+	}
+	if g.camX > mapWidth*tileSize-320 {
+		g.camX = mapWidth*tileSize - 320
+	}
+	if g.camY > mapHeight*tileSize-240 {
+		g.camY = mapHeight*tileSize - 240
 	}
 
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	// Draw the town background
-	screen.DrawImage(bgImage, nil)
+	if g.state == StateMenu {
+		screen.Fill(color.NRGBA{30, 40, 60, 255})
+		var msg string
+		msg += "Welcome to Pokemon Prototype\n\n"
+		msg += "Choose Your Character:\n"
+		msg += "[1] or [Left Arrow] for Boy\n"
+		msg += "[2] or [Right Arrow] for Girl"
+		ebitenutil.DebugPrintAt(screen, msg, 60, 50)
+
+		if playerSpriteBoy != nil {
+			opBoy := &ebiten.DrawImageOptions{}
+			opBoy.GeoM.Translate(100, 150)
+			subBoy := playerSpriteBoy.SubImage(image.Rect(0, 0, 32, 48)).(*ebiten.Image)
+			screen.DrawImage(subBoy, opBoy)
+		}
+
+		if playerSpriteGirl != nil {
+			opGirl := &ebiten.DrawImageOptions{}
+			opGirl.GeoM.Translate(180, 150)
+			subGirl := playerSpriteGirl.SubImage(image.Rect(0, 0, 32, 48)).(*ebiten.Image)
+			screen.DrawImage(subGirl, opGirl)
+		}
+		return
+	}
 
 	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(g.x, g.y)
-	
-	// Draw the player sprite based on the current direction
-	screen.DrawImage(playerSprites[g.dir], op)
+	if tilesetImage != nil {
+		for y := 0; y < mapHeight; y++ {
+			for x := 0; x < mapWidth; x++ {
+				tileID := 16
+				if y == 7 || x == 10 {
+					tileID = 17
+				}
+				vx := float64(x*tileSize) - g.camX
+				vy := float64(y*tileSize) - g.camY
+				if vx < -tileSize || vy < -tileSize || vx > 320 || vy > 240 {
+					continue
+				}
+				sx := (tileID % 8) * tileSize
+				sy := (tileID / 8) * tileSize
+				op.GeoM.Reset()
+				op.GeoM.Translate(vx, vy)
+				sub := tilesetImage.SubImage(image.Rect(sx, sy, sx+tileSize, sy+tileSize)).(*ebiten.Image)
+				screen.DrawImage(sub, op)
+			}
+		}
+	} else {
+		screen.Fill(color.NRGBA{60, 179, 113, 255})
+	}
+
+	op.GeoM.Reset()
+	screenX := g.x - g.camX
+	screenY := g.y - g.camY - 16
+	op.GeoM.Translate(screenX, screenY)
+
+	activeSprite := playerSpriteBoy
+	if g.isGirl && playerSpriteGirl != nil {
+		activeSprite = playerSpriteGirl
+	}
+
+	if activeSprite != nil {
+		sx := g.step * 32
+		sy := g.dir * 48
+		sub := activeSprite.SubImage(image.Rect(sx, sy, sx+32, sy+48)).(*ebiten.Image)
+		screen.DrawImage(sub, op)
+	}
 }
 
 func (g *Game) Layout(w, h int) (int, int) {
@@ -202,10 +245,15 @@ func (g *Game) Layout(w, h int) (int, int) {
 }
 
 func main() {
-	game := &Game{x: 144, y: 104, dir: 0}
+	game := &Game{
+		state: StateMenu,
+		x:     144,
+		y:     104,
+		dir:   0,
+	}
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
-	ebiten.SetWindowTitle("Pokemon Master - Moving Test")
+	ebiten.SetWindowTitle("Pokemon Master - Prototype")
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
